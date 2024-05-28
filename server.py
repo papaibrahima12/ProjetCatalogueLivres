8ea885a666b23b71b866a44bf959d4714215885d
papaibrahima12 <papaibrahimasow@esp.sn>
2024-05-28 16:17:16 +0000

    first commit

---
+++ server.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, jsonify
+import pandas as pd
+
+app = Flask(__name__)
+
+# Charger les fichiers Excel
+catalogues = {
+    'catalogue1': pd.read_excel('./Travail_connecteur_TDM.ods'),
+    # 'catalogue2': pd.read_excel('chemin/vers/votre/catalogue2.xlsx')
+    # Ajoutez d'autres catalogues si nécessaire
+}
+
+@app.route('/', methods=['GET'])
+def home():
+    return "Hello world"
+
+@app.route('/search', methods=['GET'])
+def search():
+    titre = request.args.get('titre')
+    auteur = request.args.get('auteur')
+    date_pub = request.args.get('date_pub')
+
+    resultats = []
+
+    for nom, catalogue in catalogues.items():
+        # Appliquer les filtres selon les paramètres de requête
+        df_result = catalogue
+        if titre:
+            df_result = df_result[df_result['TITRE'].str.contains(titre, case=False, na=False)]
+        if auteur:
+            df_result = df_result[df_result['AUTEUR'].str.contains(auteur, case=False, na=False)]
+        if date_pub:
+            df_result = df_result[df_result['ANNEE DE PUBLICATION'] == date_pub]
+        
+        resultats.extend(df_result.to_dict(orient='records'))
+
+    return jsonify(resultats)
+
+if __name__ == '__main__':
+    app.run(debug=True)
